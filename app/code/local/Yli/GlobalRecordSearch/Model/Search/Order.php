94d22b963a08f265f91c172ff3612d7aee2c8513
yuan miao <miao.yuan@ioart.com>
2015-06-05 23:43:28 +0800

    重写后台订单搜索

---
+++ app/code/local/Yli/GlobalRecordSearch/Model/Search/Order.php
@@ -0,0 +1,51 @@
+<?php 
+
+class Yli_GlobalRecordSearch_Model_Search_Order extends Mage_Adminhtml_Model_Search_Order
+{
+    public function load()
+    {
+       
+        $arr = array();
+    
+        if (!$this->hasStart() || !$this->hasLimit() || !$this->hasQuery()) {
+            $this->setResults($arr);
+            return $this;
+        }
+    
+        $query = $this->getQuery();
+        //TODO: add full name logic
+        $collection = Mage::getResourceModel('sales/order_collection')
+            ->addAttributeToSelect('*');
+            $fields = Mage::getModel('grsearch/grsearch')->getCollection()
+            ->addFieldToFilter('module','order');
+            $conditions = array();
+            foreach ($fields as $field){
+                if ($field->getIsFuzzy() == '1'){
+                    $conditions[] = array('attribute'=>$field->getField(), 'like' => $this->getQuery().'%');
+                }else{
+                    $conditions[] = array('attribute'=>$field->getField(), 'eq' => $this->getQuery());
+                }
+            
+            }
+            $collection->addAttributeToSearchFilter($conditions);
+            
+            $collection->setCurPage($this->getStart())
+            ->setPageSize($this->getLimit())
+            ->load();
+    
+        foreach ($collection as $order) {
+            $arr[] = array(
+                'id'                => 'order/1/'.$order->getId(),
+                'type'              => Mage::helper('adminhtml')->__('Order'),
+                'name'              => Mage::helper('adminhtml')->__('Order #%s', $order->getIncrementId()),
+                'description'       => $order->getBillingFirstname().' '.$order->getBillingLastname(),
+                'form_panel_title'  => Mage::helper('adminhtml')->__('Order #%s (%s)', $order->getIncrementId(), $order->getBillingFirstname().' '.$order->getBillingLastname()),
+                'url' => Mage::helper('adminhtml')->getUrl('*/sales_order/view', array('order_id'=>$order->getId())),
+            );
+        }
+    
+        $this->setResults($arr);
+    
+        return $this;
+    }
+}
